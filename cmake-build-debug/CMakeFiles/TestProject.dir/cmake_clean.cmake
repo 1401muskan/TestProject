bf40259d8786879cdf7252e778bf70c80e74ccbc
Muskan <muskan.aggarwal1401@gmail.com>
2021-09-25 15:31:48 +0530

    First commit

---
+++ cmake-build-debug/CMakeFiles/TestProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestProject.dir/main.cpp.obj"
+  "TestProject.exe"
+  "TestProject.exe.manifest"
+  "TestProject.lib"
+  "TestProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
